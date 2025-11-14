9c4985e36831230acfeab385adcb0297d10ad7de
sheine-svg <sheine@udesa.edu.ar>
2025-11-14 18:17:57 -0300

    modifiqué el product js y su html

---
+++ js/product.js
@@ -0,0 +1,61 @@
+//recuperar QS
+
+let qs = location.search;
+
+let qsObj = new URLSearchParams(qs);
+
+let idBuscado = qsObj.get("id");
+
+// URL Fetch
+
+let url = "https://dummyjson.com/products/" + idBuscado;
+
+// recuperar elementos del DOM
+
+let nombreProducto = document.querySelector(".nombreProducto");
+let marca = document.querySelector(".marca");
+let descripcion = document.querySelector(".descripcion");
+let precio = document.querySelector(".precio");
+let fotoProducto = document.querySelector(".fotoProducto");
+let categoria = document.querySelector(".categoria");
+let stock = document.querySelector(".stock");
+let tags = document.querySelector(".tags");
+
+let articleReviews = document.querySelector(".reviewsList");
+
+// fetch - 2
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        nombreProducto.innerText += data.title;
+        marca.innerText += data.brand;
+        descripcion.innerText += data.description;
+        precio.innerText += data.price;
+        fotoProducto.src = `${data.images[0]}`;
+        categoria.href += data.category; // ?? no se me agrega la categoría
+        stock.innerText += data.stock;
+        tags.innerText += data.tags;
+
+        let arrayReviews = data.reviews;
+        let reviews = "";
+
+        for (let i = 0; i < arrayReviews.length; i++) {
+            const element = arrayReviews[i];
+
+            reviews += `<article class="reviews"><p id="usuarioreviews">${element.reviewerName}</p>
+
+                        <p class="rating"><strong>Rating:</strong> ${element.rating}</p>
+
+                        <p class="fecha"><strong>Fecha:</strong> ${element.date}</p>
+
+                        <p class="comentario"><strong>Comentario:</strong> ${element.comment}</p> </article>`;
+        }
+
+        articleReviews.innerHTML = reviews;
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    })
