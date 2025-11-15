753604c3c6961322a3ca105bc9c7d86319428e23
Mpvazquez2025 <mpvazquez@udesa.edu.ar>
2025-11-15 15:23:03 -0300

    modifiqué el index js y su html

---
+++ js/index.js
@@ -0,0 +1,84 @@
+// 
+
+let url = 'https://dummyjson.com/products/category-list';
+
+// 
+
+let listaCategorias = document.querySelector(".categoriaslist");
+
+// 
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+
+    })
+
+    .then(function (data) {
+        let categorias = data;
+        let catmod = "";
+
+        for (let i = 0; i < categorias.length; i++) {
+            const element = categorias[i];
+            catmod += `<li><a href="./category.html" class="categoriaelement">${element}</a></li>`
+
+
+            listaCategorias.innerHTML = catmod;
+
+        }
+    })
+
+    .catch(function (error) {
+        console.log("Error: " + error);
+
+    })
+
+let url2 = "https://dummyjson.com/products?limit=0&skip=0"
+
+let sectionaleatorios = document.querySelector(".Aleatorios")
+let sectionmasvendidos = document.querySelector(".Masvendidos")
+
+
+fetch(url2)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        let products = data.products;
+
+        let contenido1 = ""
+        let contenido2= ""
+        for (let i = 0; i < products.length; i++) {
+            const element = products[i];
+            console.log(element)
+
+            if (element.category == "smartphones") {
+                contenido1 += `<article class="portada">
+                            <p>${element.title}</p>
+                            <img src="${element.images[0]}">
+                            <p class="descripcion">${element.description}</p>
+                            <p class="precio">$${element.price}</p>
+                        
+                            </article>`
+                sectionaleatorios.innerHTML = contenido1
+
+            } else if (element.category == "groceries") {
+                contenido2 += `<article class="portada">
+                            <p>${element.title}</p>
+                            <img src="${element.images[0]}">
+                            <p class="descripcion">${element.description}</p>
+                            <p class="precio">$${element.price}</p>
+                        
+                            </article>`
+                sectionmasvendidos.innerHTML = contenido2
+
+            }
+        }})
+
+
+
+    .catch(function (error) {
+        console.log("Error: " + error);
+
+    })
