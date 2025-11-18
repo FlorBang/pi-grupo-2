806f35a34eaef5eb757ad6837e3f8bc81c33f8cf
FlorBang <florenciabang@gmail.com>
2025-11-18 01:30:21 -0300

    Modificación de category.js y category.html

--- js/category.js
+++ js/category.js
@@ -1,31 +1,73 @@
-// 
+// ASIDE
 
 let url = 'https://dummyjson.com/products/category-list';
-
-// 
 
 let listaCategorias = document.querySelector(".categoriaslist");
 
 // 
-
 fetch(url)
     .then(function (response) {
         return response.json();
-
     })
 
     .then(function (data) {
-        let categorias = data;
         let catmod = "";
-
-        for (let i = 0; i < categorias.length; i++) {
-            const element = categorias[i];
-            catmod += `<li><a href="./category.html?id=${element.id}" class="categoriaelement">${element}</a></li>`
-
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+            catmod += `<li><a href="./category.html?category=${element}" class="categoriaelement">${element}</a></li>`
+        }
 
             listaCategorias.innerHTML = catmod;
+    
+    })
 
+    .catch(function (error) {
+        console.log("Error: " + error);
+
+    })
+
+
+// QS
+
+let params = new URLSearchParams(location.search);
+let categoriaSeleccionada = params.get("category");
+
+document.querySelector(".genre").innerText = categoriaSeleccionada.toUpperCase();
+
+// fetch URL productos según categoria
+let urlProductos = `https://dummyjson.com/products/category/${categoriaSeleccionada}`;
+
+let listaProductos = document.querySelector(".categoryList");
+
+// fetch
+
+fetch(urlProductos)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        let productos = data.products;
+        let cat = "";
+
+        for (let i = 0; i < productos.length; i++) {
+            let p = productos[i];
+            cat += `
+                
+                <article class="portada">
+                    
+                    <p>${p.title}</p>
+                    <img src="${p.images[0]}" alt="${p.title}">
+                    <p class="descripcion">${p.description}</p>
+                    <p class="precio">$${p.price}</p>
+                     
+                    <a href="./product.html?id=${p.id}" class="btncat">Ver detalle</a>
+                        
+                </article>
+              
+                `;
         }
+        listaProductos.innerHTML = cat;
     })
 
     .catch(function (error) {
