343eec46f08fede80c32cff716b3c1a2f9ddf23b
sheine-svg <sheine@udesa.edu.ar>
2025-11-21 18:05:12 -0300

    corrección del login parte 4

--- js/login.js
+++ js/login.js
@@ -1,1 +1,36 @@
+let formulario = document.querySelector(".formlogyreg");
+let camposForm = document.querySelectorAll(".camposForm");
+let password = document.querySelector("#password");
+let email = document.querySelector("#email");
+let mjeError = document.querySelector(".mjeError")
 
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (email.value === "") {
+        mjeError.innerText = "Complete el campo del mail";
+        return;
+    }
+
+    if (password.value === "") {
+        mjeError.innerText = "Complete el campo de la contraseña";
+        return;
+    }
+
+    else if (password.value.length < 6) {
+        mjeError.innerText = "Ingrese al menos 6 caracteres";
+        return;
+    }
+
+    // local storage
+    let user = {
+        email: this.email.value,
+        password: this.password.value
+    };
+
+    let userEnString = JSON.stringify(user);
+
+    localStorage.setItem("miClave", userEnString);
+
+    this.submit();
+});
